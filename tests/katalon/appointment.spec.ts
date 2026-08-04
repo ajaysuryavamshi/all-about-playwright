8ab8000d198f07cb3c8757a77a2e6a8448da23a9
Ajay Suryavamshi <ajaysuryavamshi@Ajays-MacBook-Pro.local>
2026-08-04 13:47:43 +0530

    added katalon scripts

---
+++ tests/katalon/appointment.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Make an Appointment', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://katalon-demo-cura.herokuapp.com/');
+    });
+
+    test('should be able to make an appointment', async ({ page }) => {
+        await page.getByRole('link', { name: 'Make Appointment' }).click();
+        await page.getByLabel('Username').fill('John Doe');
+        await page.getByLabel('Password').fill('ThisIsNotAPassword');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page).toHaveURL(/.*appointment/);
+
+        await page.getByLabel('Facility').selectOption('Hongkong CURA Healthcare Center');
+        await page.getByRole('checkbox', { name: 'Apply for hospital readmission' }).check();
+        await page.getByRole('radio', { name: 'Medicaid' }).check();
+        await page.getByRole('textbox', { name: 'Visit Date (Required)' }).click();
+        await page.getByRole('cell', { name: '4' }).first().click();
+        await page.getByRole('textbox', { name: 'Comment' }).fill('This is a simple comment');
+        await page.getByRole('button', { name: 'Book Appointment' }).click();
+        await expect(page.locator('h2')).toContainText('Appointment Confirmation');
+        await expect(page.locator('#summary')).toContainText('Please be informed that your appointment has been booked as following:');
+        await expect(page.locator('#facility')).toContainText('Hongkong CURA Healthcare Center');
+        await expect(page.locator('#hospital_readmission')).toContainText('Yes');
+        await expect(page.locator('#program')).toContainText('Medicaid');
+        await expect(page.locator('#visit_date')).toContainText('04/08/2026');
+        await expect(page.locator('#comment')).toContainText('This is a simple comment');
+        await page.getByRole('heading', { name: 'Appointment Confirmation' }).click();
+    });
+
+    test('should not be able to make an appointment with invalid credentials', async ({ page }) => {
+        await page.getByRole('link', { name: 'Make Appointment' }).click();
+        await page.getByLabel('Username').fill('Invalid User');
+        await page.getByLabel('Password').fill('Invalid Password');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByText('Login failed! Please ensure the username and password are valid.')).toBeVisible();
+    });
+
+    test('should not be able to make an appointment with empty credentials', async ({ page }) => {
+        await page.getByRole('link', { name: 'Make Appointment' }).click();
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByText('Login failed! Please ensure the username and password are valid.')).toBeVisible();
+    });
+
+});
